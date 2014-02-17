7a190368961eaaecf84938ed6355d889bb7ce7a3
unknown <mm@NM11-002.normatic.local>
2014-02-17 23:11:43 +0100

    Validation fixes

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://admin:admin@novus.modulusmongo.net:27017/useqYp7a' 
+	//'url' : 'mongodb://admin:admin@novus.modulusmongo.net:27017/useqYp7a' 
+	'url' : 'mongodb://pmandev:Pman79!@novus.modulusmongo.net:27017/novobU3s'
 
 };
